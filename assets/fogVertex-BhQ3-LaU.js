d16a9f62cc118f02567b88be2e46705ad605d43f
trinity publisher <admin@t27.ai>
2026-09-04 22:37:25 +0000

    Publish assets/index-oUrwI138.js from trinity d841321

---
+++ assets/fogVertex-BhQ3-LaU.js
@@ -0,0 +1,4 @@
+import{bG as o}from"./Queen-DvYDZ44X.js";const e="fogVertex",r=`#ifdef FOG
+vFogDistance=(view*worldPos).xyz;
+#endif
+`;o.IncludesShadersStore[e]||(o.IncludesShadersStore[e]=r);const t={name:e,shader:r};export{t as f};
---
+++ assets/shadowMapFragmentSoftTransparentShadow-DedsnEB_.js
@@ -0,0 +1,4 @@
+import{bG as a}from"./Queen-DvYDZ44X.js";import"./index-oUrwI138.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-DHWxIZD9.js";const r="shadowMapFragmentSoftTransparentShadow",o=`#if SM_SOFTTRANSPARENTSHADOW==1
+if ((bayerDither8(floor(mod(gl_FragCoord.xy,8.0))))/64.0>=softTransparentShadowSM.x*alpha) discard;
+#endif
+`;a.IncludesShadersStore[r]||(a.IncludesShadersStore[r]=o);const n={name:r,shader:o};export{n as shadowMapFragmentSoftTransparentShadow};
