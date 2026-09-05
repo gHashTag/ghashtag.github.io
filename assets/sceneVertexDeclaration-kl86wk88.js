e9e99fecc35d1ee3e47a4f26c7d77302d5df9a41
trinity publisher <admin@t27.ai>
2026-09-05 16:12:04 +0000

    Publish assets/index-BLhDdMEg.js from trinity 44b067e

---
+++ assets/sceneVertexDeclaration-kl86wk88.js
@@ -0,0 +1,6 @@
+import{bM as o}from"./Queen-CMaJNJEY.js";const e="sceneVertexDeclaration",r=`uniform mat4 viewProjection;
+#ifdef MULTIVIEW
+uniform mat4 viewProjectionR;
+#endif
+uniform mat4 view;uniform mat4 projection;uniform vec4 vEyePosition;
+`;o.IncludesShadersStore[e]||(o.IncludesShadersStore[e]=r);const t={name:e,shader:r};export{t as s};
---
+++ assets/shadowMapFragmentSoftTransparentShadow-d8sTQ24M.js
@@ -0,0 +1,4 @@
+import{bM as a}from"./Queen-CMaJNJEY.js";import"./index-BLhDdMEg.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-DHWxIZD9.js";const r="shadowMapFragmentSoftTransparentShadow",o=`#if SM_SOFTTRANSPARENTSHADOW==1
+if ((bayerDither8(floor(mod(gl_FragCoord.xy,8.0))))/64.0>=softTransparentShadowSM.x*alpha) discard;
+#endif
+`;a.IncludesShadersStore[r]||(a.IncludesShadersStore[r]=o);const n={name:r,shader:o};export{n as shadowMapFragmentSoftTransparentShadow};
