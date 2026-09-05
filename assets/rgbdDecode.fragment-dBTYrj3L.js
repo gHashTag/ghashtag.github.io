9a70bd6e0611e118a9b50a293bc80ffaa10cd0fd
trinity publisher <admin@t27.ai>
2026-09-05 01:13:17 +0000

    Publish assets/index-B8mCFqSL.js from trinity d113bfe

---
+++ assets/rgbdDecode.fragment-dBTYrj3L.js
@@ -0,0 +1,5 @@
+import{bG as e}from"./Queen-DbI94jLB.js";import{h as i}from"./helperFunctions--sznzeRs.js";import"./index-B8mCFqSL.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-DHWxIZD9.js";const o="rgbdDecodePixelShader",t=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[o]||(e.ShadersStore[o]=t);const n=[i];for(const r of n)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const l={name:o,shader:t};export{l as rgbdDecodePixelShader};
