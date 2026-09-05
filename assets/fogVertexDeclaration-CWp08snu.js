ca3e95f344cb379b53aa3d1a1c8c5ec1a1950dc7
trinity publisher <admin@t27.ai>
2026-09-05 20:17:41 +0000

    Publish assets/index-9DIKHrFJ.js from trinity bec6192

---
+++ assets/fogVertexDeclaration-CWp08snu.js
@@ -0,0 +1,4 @@
+import{bO as r}from"./Queen-DSkuV_jW.js";const e="fogVertexDeclaration",o=`#ifdef FOG
+varying vec3 vFogDistance;
+#endif
+`;r.IncludesShadersStore[e]||(r.IncludesShadersStore[e]=o);const t={name:e,shader:o};export{t as f};
---
+++ assets/logDepthDeclaration-Ck4EP1E7.js
@@ -0,0 +1,4 @@
+import{bO as t}from"./Queen-DSkuV_jW.js";const e="logDepthDeclaration",o=`#ifdef LOGARITHMICDEPTH
+uniform float logarithmicDepthConstant;varying float vFragmentDepth;
+#endif
+`;t.IncludesShadersStore[e]||(t.IncludesShadersStore[e]=o);const r={name:e,shader:o};export{r as l};
