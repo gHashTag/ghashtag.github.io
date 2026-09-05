1035491347556bd6ddb335a8fe4d8645880608f4
trinity publisher <admin@t27.ai>
2026-09-05 21:16:55 +0000

    Publish assets/index-dS6beG53.js from trinity 1df4f87

---
+++ assets/kernelBlurVaryingDeclaration-CHAvWGYW.js
@@ -0,0 +1,1 @@
+import{bO as e}from"./Queen-B6TvHqg_.js";const r="kernelBlurVaryingDeclaration",a="varying sampleCoord{X}: vec2f;";e.IncludesShadersStoreWGSL[r]||(e.IncludesShadersStoreWGSL[r]=a);const o={name:r,shader:a};export{o as k};
---
+++ assets/sceneVertexDeclaration-CAJYkwuA.js
@@ -0,0 +1,6 @@
+import{bO as o}from"./Queen-B6TvHqg_.js";const e="sceneVertexDeclaration",r=`uniform mat4 viewProjection;
+#ifdef MULTIVIEW
+uniform mat4 viewProjectionR;
+#endif
+uniform mat4 view;uniform mat4 projection;uniform vec4 vEyePosition;
+`;o.IncludesShadersStore[e]||(o.IncludesShadersStore[e]=r);const t={name:e,shader:r};export{t as s};
