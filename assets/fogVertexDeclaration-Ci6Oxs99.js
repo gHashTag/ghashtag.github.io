8554713ad6653f0d96b017e9183b039a1031e495
trinity publisher <admin@t27.ai>
2026-09-05 19:13:38 +0000

    Publish assets/index-D5jq6Sjl.js from trinity c05573c

---
+++ assets/fogVertexDeclaration-Ci6Oxs99.js
@@ -0,0 +1,4 @@
+import{bO as r}from"./Queen-C1ef3QGQ.js";const e="fogVertexDeclaration",o=`#ifdef FOG
+varying vFogDistance: vec3f;
+#endif
+`;r.IncludesShadersStoreWGSL[e]||(r.IncludesShadersStoreWGSL[e]=o);const t={name:e,shader:o};export{t as f};
