1c32ca6da05b7462a534efdea37b064d50bb0de4
trinity publisher <admin@t27.ai>
2026-09-05 01:51:57 +0000

    Publish assets/index-B7dNkyhj.js from trinity d882cdf

---
+++ assets/fogVertex-DG6pG6cL.js
@@ -0,0 +1,8 @@
+import{bG as s}from"./Queen-FqNpciL_.js";const e="fogVertex",o=`#ifdef FOG
+#ifdef SCENE_UBO
+vertexOutputs.vFogDistance=(scene.view*worldPos).xyz;
+#else
+vertexOutputs.vFogDistance=(uniforms.view*worldPos).xyz;
+#endif
+#endif
+`;s.IncludesShadersStoreWGSL[e]||(s.IncludesShadersStoreWGSL[e]=o);const r={name:e,shader:o};export{r as f};
---
+++ assets/fogVertexDeclaration-BBkyJ8Jp.js
@@ -0,0 +1,4 @@
+import{bG as r}from"./Queen-FqNpciL_.js";const e="fogVertexDeclaration",o=`#ifdef FOG
+varying vFogDistance: vec3f;
+#endif
+`;r.IncludesShadersStoreWGSL[e]||(r.IncludesShadersStoreWGSL[e]=o);const t={name:e,shader:o};export{t as f};
