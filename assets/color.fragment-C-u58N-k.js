1581491ad4cd1faab0e66b4130bb05cca1ceafcf
trinity publisher <admin@t27.ai>
2026-09-06 00:32:43 +0000

    Publish assets/index-Yb5zHw8F.js from trinity c9a1dae

---
+++ assets/color.fragment-C-u58N-k.js
@@ -0,0 +1,20 @@
+import{bY as e}from"./Queen-nAuG3tsK.js";import{c as a,a as i}from"./clipPlaneFragment-D5g9orWt.js";import{f as d,a as l}from"./fogFragment-Bb4ej2iJ.js";import"./index-Yb5zHw8F.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-DHWxIZD9.js";const r="colorPixelShader",n=`#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+#define VERTEXCOLOR
+varying vec4 vColor;
+#else
+uniform vec4 color;
+#endif
+#include<clipPlaneFragmentDeclaration>
+#include<fogFragmentDeclaration>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+#include<clipPlaneFragment>
+#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+gl_FragColor=vColor;
+#else
+gl_FragColor=color;
+#endif
+#include<fogFragment>(color,gl_FragColor)
+#define CUSTOM_FRAGMENT_MAIN_END
+}`;e.ShadersStore[r]||(e.ShadersStore[r]=n);const t=[a,d,i,l];for(const o of t)e.IncludesShadersStore[o.name]||(e.IncludesShadersStore[o.name]=o.shader);const N={name:r,shader:n};export{N as colorPixelShader};
