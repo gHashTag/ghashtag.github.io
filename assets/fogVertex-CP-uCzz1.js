9901c929ea0fb3c48473cbc8eda42c78e979d37f
trinity publisher <admin@t27.ai>
2026-09-06 09:38:44 +0000

    Publish assets/index-Cc9egyE_.js from trinity b7bc7f5

---
+++ assets/fogVertex-CP-uCzz1.js
@@ -0,0 +1,4 @@
+import{bS as o}from"./Queen-DhSf2O0T.js";const e="fogVertex",r=`#ifdef FOG
+vFogDistance=(view*worldPos).xyz;
+#endif
+`;o.IncludesShadersStore[e]||(o.IncludesShadersStore[e]=r);const t={name:e,shader:r};export{t as f};
---
+++ assets/rgbdEncode.fragment-BXb8brlN.js
@@ -0,0 +1,5 @@
+import{bS as e}from"./Queen-DhSf2O0T.js";import{h as n}from"./helperFunctions-D_oVK7-F.js";import"./index-Cc9egyE_.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-DHWxIZD9.js";const o="rgbdEncodePixelShader",t=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=toRGBD(texture2D(textureSampler,vUV).rgb);}`;e.ShadersStore[o]||(e.ShadersStore[o]=t);const i=[n];for(const r of i)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const l={name:o,shader:t};export{l as rgbdEncodePixelShader};
