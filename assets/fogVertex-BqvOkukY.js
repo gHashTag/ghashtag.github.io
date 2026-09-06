13e1db434fa6830f598f50eeaa2693c78bc45561
trinity publisher <admin@t27.ai>
2026-09-06 04:09:44 +0000

    Publish assets/index-DJt6CJOG.js from trinity 6024109

---
+++ assets/fogVertex-BqvOkukY.js
@@ -0,0 +1,4 @@
+import{bY as o}from"./Queen-CHuCUg0D.js";const e="fogVertex",r=`#ifdef FOG
+vFogDistance=(view*worldPos).xyz;
+#endif
+`;o.IncludesShadersStore[e]||(o.IncludesShadersStore[e]=r);const t={name:e,shader:r};export{t as f};
---
+++ assets/rgbdDecode.fragment-BQKF1Ppw.js
@@ -0,0 +1,5 @@
+import{bY as e}from"./Queen-CHuCUg0D.js";import{h as i}from"./helperFunctions-C9fS8foL.js";import"./index-DJt6CJOG.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-DHWxIZD9.js";const o="rgbdDecodePixelShader",t=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[o]||(e.ShadersStore[o]=t);const n=[i];for(const r of n)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const l={name:o,shader:t};export{l as rgbdDecodePixelShader};
