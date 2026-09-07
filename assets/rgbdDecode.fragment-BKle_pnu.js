96f5481089bf616ed7f963a13a56ef347debcfba
trinity publisher <admin@t27.ai>
2026-09-07 11:45:24 +0000

    Publish assets/index-D5f5alzV.js from trinity ab8c2c4

---
+++ assets/rgbdDecode.fragment-BKle_pnu.js
@@ -0,0 +1,5 @@
+import{aB as e}from"./Queen-DkVys0TV.js";import{h as n}from"./helperFunctions-BTD9Fpa3.js";import"./index-D5f5alzV.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-DHWxIZD9.js";const t="rgbdDecodePixelShader",a=`varying vUV: vec2f;var textureSamplerSampler: sampler;var textureSampler: texture_2d<f32>;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {fragmentOutputs.color=vec4f(fromRGBD(textureSample(textureSampler,textureSamplerSampler,input.vUV)),1.0);}`;e.ShadersStoreWGSL[t]||(e.ShadersStoreWGSL[t]=a);const o=[n];for(const r of o)e.IncludesShadersStoreWGSL[r.name]||(e.IncludesShadersStoreWGSL[r.name]=r.shader);const u={name:t,shader:a};export{u as rgbdDecodePixelShaderWGSL};
