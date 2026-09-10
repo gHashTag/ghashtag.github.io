6d2a976b85af263b5bc5b217c2580045f7d3f47f
trinity publisher <admin@t27.ai>
2026-09-10 06:39:25 +0000

    Publish assets/index-DnqmecgZ.js from trinity a368d81

---
+++ assets/kernelBlurVaryingDeclaration-D9ev55Qv.js
@@ -0,0 +1,1 @@
+import{ar as e}from"./QueenComb-DecC014n.js";const r="kernelBlurVaryingDeclaration",a="varying vec2 sampleCoord{X};";e.IncludesShadersStore[r]||(e.IncludesShadersStore[r]=a);const o={name:r,shader:a};export{o as k};
