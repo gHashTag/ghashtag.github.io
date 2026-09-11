0dfea101b0d414cfa01305401dc3866fa7db19f3
trinity publisher <admin@t27.ai>
2026-09-11 15:17:51 +0000

    Publish assets/index-bKj3lGZ0.js from trinity 03ae2f9

---
+++ assets/color.fragment-DGkMMhDb.js
@@ -0,0 +1,21 @@
+import{ar as e}from"./QueenComb-B2NVP5Qn.js";import{c as a,a as t}from"./clipPlaneFragment-B2cfGlX5.js";import{f as i,a as f}from"./fogFragment-C1I2dhsg.js";import"./index-bKj3lGZ0.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-CAi4bOxy.js";import"./queenRepositoryWorld-DQIYnztg.js";const n="colorPixelShader",o=`#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+#define VERTEXCOLOR
+varying vColor: vec4f;
+#else
+uniform color: vec4f;
+#endif
+#include<clipPlaneFragmentDeclaration>
+#include<fogFragmentDeclaration>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+#include<clipPlaneFragment>
+#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+fragmentOutputs.color=input.vColor;
+#else
+fragmentOutputs.color=uniforms.color;
+#endif
+#include<fogFragment>(color,fragmentOutputs.color)
+#define CUSTOM_FRAGMENT_MAIN_END
+}`;e.ShadersStoreWGSL[n]||(e.ShadersStoreWGSL[n]=o);const d=[a,i,t,f];for(const r of d)e.IncludesShadersStoreWGSL[r.name]||(e.IncludesShadersStoreWGSL[r.name]=r.shader);const O={name:n,shader:o};export{O as colorPixelShaderWGSL};
