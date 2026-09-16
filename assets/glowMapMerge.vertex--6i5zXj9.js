82202bf87e6d152da8b8559fb4178a1fb511e29f
Dmitrii Vasilev <1turbobaby@gmail.com>
2026-09-16 21:13:27 +0700

    Publish assets/index-3zHDZIRR.js from trinity b737b99

---
+++ assets/glowMapMerge.vertex--6i5zXj9.js
@@ -0,0 +1,7 @@
+import{ar as o}from"./QueenComb-DojR2dWW.js";import"./index-3zHDZIRR.js";import"./react-D-uFLJMr.js";import"./motion-mybjp1Q4.js";import"./router-4H5ws2tv.js";import"./queenRepositoryWorld-CyuPyfZ9.js";const e="glowMapMergeVertexShader",r=`attribute vec2 position;varying vec2 vUV;const vec2 madd=vec2(0.5,0.5);
+#define CUSTOM_VERTEX_DEFINITIONS
+void main(void) {
+#define CUSTOM_VERTEX_MAIN_BEGIN
+vUV=position*madd+madd;gl_Position=vec4(position,0.0,1.0);
+#define CUSTOM_VERTEX_MAIN_END
+}`;o.ShadersStore[e]||(o.ShadersStore[e]=r);const p={name:e,shader:r};export{p as glowMapMergeVertexShader};
