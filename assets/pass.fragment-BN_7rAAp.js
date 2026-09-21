d83be5814353b90cafd18fd9dc85c8ec9fa9bc4f
trinity publisher <admin@t27.ai>
2026-09-21 18:46:35 +0000

    Publish assets/index-BHJ2K2_V.js from trinity a39c553

---
+++ assets/pass.fragment-BN_7rAAp.js
@@ -0,0 +1,4 @@
+import{ar as e}from"./QueenComb-CjKLSiO3.js";import"./index-BHJ2K2_V.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-CAi4bOxy.js";const r="passPixelShader",o=`varying vec2 vUV;uniform sampler2D textureSampler;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=texture2D(textureSampler,vUV);}`;e.ShadersStore[r]||(e.ShadersStore[r]=o);const S={name:r,shader:o};export{S as passPixelShader};
