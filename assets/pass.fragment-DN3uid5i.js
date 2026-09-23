64420fcbe2c0b2633690c3df9b88777c39bc7f53
trinity publisher <admin@t27.ai>
2026-09-23 06:26:30 +0000

    Publish assets/index-mVyYC260.js from trinity 69aaeee

---
+++ assets/pass.fragment-DN3uid5i.js
@@ -0,0 +1,4 @@
+import{ar as e}from"./QueenComb-HnC-hR6m.js";import"./index-mVyYC260.js";import"./react-BikoVsHo.js";import"./motion-DmZWFm6O.js";import"./router-CAi4bOxy.js";const r="passPixelShader",o=`varying vec2 vUV;uniform sampler2D textureSampler;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=texture2D(textureSampler,vUV);}`;e.ShadersStore[r]||(e.ShadersStore[r]=o);const S={name:r,shader:o};export{S as passPixelShader};
